90835cbe3d236c5e3b874938b31c41ad223ed334
oconnalla <oconnalla@gmail.com>
2019-04-10 15:29:30 -0700

    started portal pages, css having issues with sizing and running over elements

---
+++ portalPage/portal.php
@@ -0,0 +1,17 @@
+<?php
+include 'includes/header.php';
+?>
+        
+            <div id="aboutMeDiv">
+                <img id ="arielPhoto" src="images/ariel.png" alt="Ariel">
+                <h1 id="aboutMeH1">About Me</h1>
+                <!-- <img src="images/ariel.png" alt="Ariel"> -->
+                <p id="myLanguagesP"> █ Javascript &nbsp; █ CSS &nbsp; █ HTML &nbsp; ▅ Python &nbsp; ▃ SQL &nbsp; ▃ PHP</p>
+                <p>I'm a computer programming student with a focus in UX Design and Web Development. I have a Bachelors in Psychology from Seattle Pacific University, where I did a moderate amount of lab research.</br> 
+                Currently, I work as an LMT, and I love the challenges that come with injury specific treatment. Like programming, it's a process of using available information (symptoms within the entire body system), logic (discerning injury specific symptoms, treatment options), and problem solving (forming treatment plans). I like that web development feels like a very similar process. I'm passionate about the design process, I'm particularly fond of CSS.</p>
+            </div>
+            <img id="devJoke" src="images/developerjoke.png" alt="Developer Joke">
+            
+            <?php
+            include 'includes/footer.php';
+            ?>
